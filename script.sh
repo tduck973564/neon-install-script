81f0a32e48d333579c1df2ffd351f54a744c207c
tduck973564 <tduck973564@gmail.com>
2022-06-18 01:27:36 +1000

    fixed onedrive

--- script.sh
+++ script.sh
@@ -102,8 +102,9 @@ mkdir ~/Coding
 mkdir ~/Games
 
 echo Install OneDrive
-wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_22.04/ ./" | sudo tee /etc/apt/sources.list.d/onedrive.list
+wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_20.04/Release.key | sudo apt-key add -
+echo 'deb https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_20.04/ ./' | sudo tee /etc/apt/sources.list.d/onedrive.list
+sudo apt update
 sudo pkcon install -y onedrive
 onedrive
 systemctl --user enable onedrive
